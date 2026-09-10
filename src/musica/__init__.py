81c5401da57feb93d2c2842e3c8d93b56933e93e
Eckommon <lekymo@gmail.com>
2026-09-11 01:59:17 +0900

    M0-R2: expose minimal runtime core API

--- src/musica/__init__.py
+++ src/musica/__init__.py
@@ -1,5 +1,21 @@
-"""MUSICA core contracts / MUSICA 코어 계약."""
+"""MUSICA core / MUSICA 코어."""
 
+from .compiler import compile_blueprint
 from .contracts import ContractError, RevisionConflict, validate_contract, validate_revision
+from .diff import structured_diff
+from .render import midi_bytes, render_midi, render_wav, wav_bytes
+from .semantic import apply_semantic_control
 
-__all__ = ["ContractError", "RevisionConflict", "validate_contract", "validate_revision"]
+__all__ = [
+    "ContractError",
+    "RevisionConflict",
+    "apply_semantic_control",
+    "compile_blueprint",
+    "midi_bytes",
+    "render_midi",
+    "render_wav",
+    "structured_diff",
+    "validate_contract",
+    "validate_revision",
+    "wav_bytes",
+]
